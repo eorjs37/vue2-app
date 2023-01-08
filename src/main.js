39ef36c7e69f84767d4c4daa1774b1482b406bc1
macdaegeon <chleorjs37@gmail.com>
2023-01-08 17:20:11 +0900

    header 컴퍼넌트 진행

--- src/main.js
+++ src/main.js
@@ -2,9 +2,10 @@ import Vue from "vue";
 import App from "./App.vue";
 
 import VueCarousel from "vue-carousel";
-
+import HeaderComponent from "./components/HeaderComponent";
 Vue.config.productionTip = false;
 Vue.use(VueCarousel);
+Vue.component("Header", HeaderComponent);
 new Vue({
   render: (h) => h(App),
 }).$mount("#app");
